10cbfd0a9465f908481da4840341e5ea10b57871
Vincent Hanquez <vincent@coretype.io>
2023-12-27 02:23:26 +0800

    add test

--- werbolg-tests/src/lib.rs
+++ werbolg-tests/src/lib.rs
@@ -19,10 +19,25 @@ fn module1() -> werbolg_core::Module {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use alloc::{vec, vec::Vec};
+    use werbolg_compile::{compile, CompilationError, CompilationParams, Environment};
+
+    fn literal_mapper(
+        lit: werbolg_core::Literal,
+    ) -> Result<werbolg_core::Literal, CompilationError> {
+        Ok(lit)
+    }
 
     #[test]
     fn it_compiles() {
         let mod1 = module1();
-        assert!(false)
+        let params = CompilationParams { literal_mapper };
+        let mut environ = Environment::<(), ()>::new();
+        let r = compile(
+            &params,
+            vec![(werbolg_core::Namespace::root(), mod1)],
+            &mut environ,
+        );
+        assert!(r.is_ok(), "{:?}", r.err())
     }
 }
